5ef8c08bc6f30db25ebbd70e4c98c838ecc46ec8
Hugo "ThePooN" Denizart <thepoon@cartooncraft.fr>
2017-12-06 13:48:30 +0100

    CI: I happen to be ridiculous sometimes.

--- ci/add-config.js
+++ ci/add-config.js
@@ -1,17 +1,13 @@
 if(!process.env["IRC_USER"] || !process.env["IRC_PASS"])
 	throw new Error("Environment variables not correctly set!");
 
-const getConfig = () => {
-	const config = {
-		"irc_user": process.env["IRC_USER"],
-		"irc_pass": process.env["IRC_PASS"]
-	};
-	if(process.env["IRC_HOST"])
-		config["irc_host"] = process.env["IRC_HOST"];
-	if(process.env["IRC_PORT"])
-		config["irc_port"] = process.env["IRC_PORT"];
-	
-	return config;
+const config = {
+	"irc_user": process.env["IRC_USER"],
+	"irc_pass": process.env["IRC_PASS"]
 };
+if(process.env["IRC_HOST"])
+	config["irc_host"] = process.env["IRC_HOST"];
+if(process.env["IRC_PORT"])
+	config["irc_port"] = parseInt(process.env["IRC_PORT"]);
 
-require("fs").writeFileSync("./config.json", JSON.stringify(getConfig()));
+require("fs").writeFileSync("./config.json", JSON.stringify(config));
