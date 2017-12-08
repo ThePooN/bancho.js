a934b28ecd14b97bae6a802f7c22d8b0126f0849
Hugo "ThePooN" Denizart <thepoon@cartooncraft.fr>
2017-12-08 19:59:08 +0100

    Fix ConnectStates description on documentation

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 /**
  * Main Banchojs class
  * @prop {BanchoClient} BanchoClient The BanchoClient class (Client alias is kept for history purposes)
- * @prop {ConnectStates} ConnectStates The BanchoClient class (Client alias is kept for history purposes)
- */
+ * @prop {ConnectStates} ConnectStates An enum containing all the possible ConnectStates of bancho.js */
 class Banchojs {
 	constructor() {
 		this.Client = this.BanchoClient = require("./lib/BanchoClient");
