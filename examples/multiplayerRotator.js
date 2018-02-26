15f1e4026f369e88a4323e96b25cb9715428cdb3
Hugo "ThePooN" Denizart <thepoon@cartooncraft.fr>
2018-02-26 16:51:50 +0100

    Add multiplayerRotator example

---
+++ examples/multiplayerRotator.js
@@ -0,0 +1,34 @@
+const config = require("../config.json");
+const Banchojs = require(".."); // Replace .. by bancho.js when coding outside of the library
+const client = new Banchojs.BanchoClient(config["irc_user"], config["irc_pass"], config["irc_host"], config["irc_port"], config["api_key"]);
+
+const beatmaps = [75, 1262832, 714001, 1378285, 1385398, 1373950];
+let currentBeatmapIndex = 0;
+let lobby;
+
+client.connect().then(async () => {
+	console.log("We're online!");
+	const channel = await client.createLobby("Multiplayer rotator "+Math.random().toString(36).substring(8));
+	lobby = channel.lobby;
+	const password = Math.random().toString(36).substring(8);
+	await Promise.all([lobby.setPassword(password), lobby.setMap(beatmaps[currentBeatmapIndex])]);
+	console.log("Lobby created! Name: "+lobby.name+", password: "+password);
+	console.log("Multiplayer link: https://osu.ppy.sh/mp/"+lobby.id);
+
+	lobby.on("playerJoined", (obj) => {
+		if(obj.player.user.isClient())
+			lobby.setHost("#"+obj.player.user.id);
+	});
+	lobby.on("matchFinished", () => {
+		currentBeatmapIndex++;
+		if(currentBeatmapIndex == beatmaps.length)
+			currentBeatmapIndex = 0;
+		lobby.setMap(beatmaps[currentBeatmapIndex]);
+	});
+}).catch(console.error);
+
+process.on("SIGINT", async () => {
+	console.log("Closing lobby and disconnecting...");
+	await lobby.closeLobby();
+	await client.disconnect();
+});
