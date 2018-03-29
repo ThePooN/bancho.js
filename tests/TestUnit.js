af13a900f1d3f524787d59ef73b5606b9f43637e
Hugo "ThePooN" Denizart <thepoon@cartooncraft.fr>
2018-03-29 14:04:32 +0200

    Add asserts in TestUnit

--- tests/TestUnit.js
+++ tests/TestUnit.js
@@ -16,6 +16,25 @@ class TestUnit {
 	run() {
 		throw new Error("Unimplemented unit!");
 	}
+	
+	assertTrue(obj) {
+		this.assertEquals(true, obj);
+	}
+	assertFalse(obj) {
+		this.assertEquals(false, obj);
+	}
+	assertEquals(expected, obj) {
+		if(obj !== expected)
+			throw new Error("Invalid value: expected "+expected+", got "+obj+"!");
+	}
+	assertArrayEquals(expected, obj) {
+		if(expected.length != obj.length)
+			throw new Error("Invalid value: expected an array of "+expected.length+" objects, got "+obj.length+"!");
+		for(const index in expected)
+			if(expected.hasOwnProperty(index))
+				if(expected[index] != obj[index])
+					throw new Error("Invalid value at index "+index+": expected "+expected[index]+", got "+obj[index]+"!");
+	}
 }
 
 module.exports.TestUnit = TestUnit;
