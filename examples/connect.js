6ede23f7c7fcd65a735cc483087eaebe6b95c814
Willow Snow <willow.snow14@gmail.com>
2018-07-19 07:34:30 -0500

    Prefer template literals to string concat

--- examples/connect.js
+++ examples/connect.js
@@ -4,6 +4,6 @@ const client = new Banchojs.BanchoClient(require("../config.json"));
 client.connect().then(() => {
 	console.log("We're online! Now listening for incoming messages.");
 	client.on("PM", (message) => {
-		console.log(message.user.ircUsername+": "+message.message);
+		console.log(`${message.user.ircUsername}: ${message.message}`);
 	});
 }).catch(console.error);
