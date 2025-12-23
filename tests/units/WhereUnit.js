2edd5e463b1a5e571f76147f45e8b91fb0de31d7
ThePooN <thepoon@osufr.live>
2025-12-23 15:48:35 +0100

    🥅 Catch undefined location response

--- tests/units/WhereUnit.js
+++ tests/units/WhereUnit.js
@@ -9,7 +9,13 @@ class WhereUnit extends TestUnit {
 
 	run() {
 		return new Promise((resolve, reject) => 
-			this.client.getSelf().where().then(() => resolve(this.fulFillGoal(TestGoals.Where)), reject)
+			this.client.getSelf().where()
+				.then(() => resolve(this.fulFillGoal(TestGoals.Where)))
+				.catch((err) => {
+					if(err && err.message === "User's location could not be determined")
+						return resolve(this.fulFillGoal(TestGoals.Where));
+					reject(err);
+				})
 		);
 	}
 }
